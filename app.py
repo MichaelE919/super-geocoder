79e30ec34e6c626aa977bd22ebc96b2f9f0dc378
Michael Eichenberger <mikeetpt@gmail.com>
2017-09-07 00:27:51 -0500

    Add df.to_html method and sync up code with solution

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ then presents that dataset back to the user with additional values for latitude
 and longitude. The user has the option to download an updated CSV file.
 """
 import pandas
+import datetime
 from geopy.geocoders import Nominatim
 
 from flask import Flask, render_template, request, send_file
-from werkzeug import secure_filename
 
 app = Flask(__name__)
 
@@ -38,28 +38,30 @@ def index():
 @app.route('/success-table', methods=['POST'])
 def success():
     """Collect data entered on form and commit to database."""
-    global file
+    global filename
     if request.method == 'POST':
         file = request.files['file']
-        file.save(secure_filename('uploaded' + file.filename))
-        # Create a dataframe object (pandas)
-        df = pandas.read_csv('uploaded' + file.filename)
-        if 'Address' and 'address' in df.columns:
+        df = pandas.read_csv(file)
+        try:
             geocoder(df)
-            print(df)
-            return render_template('index.html', btn='download.html')
-    return render_template(
-        'index.html',
-        text='Please make sure you have an address column in your CSV file!')
+            filename = datetime.datetime.now().strftime(
+                'upload_files/%Y-%m-%d-%H-%M-%S-%f' + '.csv')
+            df.to_csv(filename, index=None)
+            return render_template(
+                'index.html', text=df.to_html(), btn='download.html')
+        except KeyError:
+            return render_template(
+                'index.html',
+                text=
+                'Please make sure you have an address column in your CSV file!'
+            )
 
 
 @app.route('/download')
 def download():
     """Download selected file as attachment to user."""
     return send_file(
-        'uploaded' + file.filename,
-        attachment_filename='yourfile.txt',
-        as_attachment=True)
+        filename, attachment_filename='yourfile.csv', as_attachment=True)
 
 
 if __name__ == '__main__':
